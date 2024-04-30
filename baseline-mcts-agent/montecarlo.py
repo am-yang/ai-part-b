9bf6f110894d1d5a522fb043a162811cc1f64da5
Anita <anita.yang.1@unimelb.edu.au>
2024-04-30 13:55:10 +0000

    More debug

--- baseline-mcts-agent/montecarlo.py
+++ baseline-mcts-agent/montecarlo.py
@@ -4,11 +4,11 @@ from .moves import all_possible_moves, is_terminal_state, apply_move, count_colo
 from referee.game import PlayerColor, Coord, PlaceAction
 
 UCB_CONSTANT = 1
-TIME_LIMIT = 100
+TIME_LIMIT = 50
 
 class MCTSNode:
 
-    def __init__(self, color: PlayerColor, state: dict[Coord, PlayerColor], depth: int, is_first_move: bool = False, parent=None, parent_action=None):
+    def __init__(self, color: PlayerColor, state: dict[Coord, PlayerColor], depth: int, parent=None, parent_action=None):
         self.color: PlayerColor = color
         self.state: dict[Coord, PlayerColor]= state
         self.parent: MCTSNode = parent
@@ -18,38 +18,37 @@ class MCTSNode:
         self.num_visits: int = 0
         self.depth: int = depth
         self.ucb_score: float = 0
-        self.is_first_move = is_first_move
         return
     
 
 def calculate_ucb(node: MCTSNode) -> float:
-    if node.visited_count == 0:
+    if node.num_visits == 0:
         return float('inf')
     
     if node.parent:
-        return (node.wins / float(node.visited_count)) + UCB_CONSTANT * math.sqrt(math.log(node.parent.visited_count) / float(node.visited_count)) 
+        return (node.wins / float(node.num_visits)) + UCB_CONSTANT * math.sqrt(math.log(node.parent.num_visits) / float(node.num_visits)) 
     else:
-        return (node.wins / float(node.visited_count)) + UCB_CONSTANT * math.sqrt(math.log(node.parent.visited_count) / float(node.visited_count)) 
+        return (node.wins / float(node.num_visits)) + UCB_CONSTANT * math.sqrt(math.log(node.parent.num_visits) / float(node.num_visits)) 
 
 
 def init_children(node: MCTSNode):
-    if is_terminal_state(node.state, node.depth):
+    if is_terminal_state(node.state, node.color, node.depth):
         return None
     
     # Array that stores info about the current state's children
     child_nodes: list[MCTSNode] = []
-    curr_depth = 1 + node.depth
+    child_depth = 1 + node.depth
     child_player = node.color
-    if node.state != {} and node.color == PlayerColor.RED:
+    if node.depth != 0 and node.color == PlayerColor.RED:
         child_player = PlayerColor.BLUE
-    elif node.state != {} and node.color == PlayerColor.BLUE:
+    elif node.depth != 0 and node.color == PlayerColor.BLUE:
         child_player = PlayerColor.RED
     
-    possible_actions = all_possible_moves(node.state, curr_depth, child_player)
+    possible_actions = all_possible_moves(node.state, child_depth, child_player)
 
     for action in possible_actions:
         board = apply_move(node.state, action, child_player)
-        child_nodes.append(MCTSNode(color=child_player, state=board, depth=curr_depth, parent=node, parent_action=action))
+        child_nodes.append(MCTSNode(color=child_player, state=board, depth=child_depth, parent=node, parent_action=action))
 
     node.children = child_nodes
 
@@ -61,35 +60,38 @@ def simulation(
     '''
     board: list[Coord, PlayerColor] = {key: value for key, value in node.state.items()}
     depth: int = node.depth
-    curr_player = node.turn # also keep track of whose turn it is
+    curr_player = node.color # also keep track of whose turn it is
     self_won = False
 
     while not is_terminal_state(board, node.color, depth):
-        
+        print(render(board, use_color=True))
+        print("depth: " + str(depth) + ", color: " + str(curr_player))
+
+        # Alternate turns
+        curr_player = PlayerColor.BLUE if curr_player == PlayerColor.RED else PlayerColor.RED
+        depth += 1
         possible_actions = all_possible_moves(board, depth, curr_player)
 
         random_action = random.choice(possible_actions)
 
-        board = apply_move(board, random_action)
+        board = apply_move(board, random_action, curr_player)
 
-        # Alternate turns
-        curr_player = PlayerColor.BLUE if curr_player == PlayerColor.RED else PlayerColor.RED
-
-        depth += 1
     
     # Check who won the simulation 
     if depth == 150:
-        count = count_colors(node.turn)
+        count = count_colors(node.color)
         if count > 75:
             self_won = True
     else:
         # if the game ended on a player's turn (i.e., before they could make another move), then the opponent wins
-        if curr_player != node.turn:
+        if curr_player != node.color:
             self_won = True
 
     if self_won:
+        print(str(node.color) + " WON this simulation")
         return 1
     else:
+        print(str(node.color) + " LOST this simulation")
         return 0
 
 def selection(
@@ -100,10 +102,13 @@ def selection(
     while curr_node.children:
         children = curr_node.children
         max_child = children[0] 
+        max_ucb = calculate_ucb(max_child)
         # Find the child with the highest UCB value
         for child in children:
-            if child.calculate_ucb() > max_child.calculate_ucb():
+            child_ucb = calculate_ucb(child)
+            if child_ucb > max_ucb:
                 max_child = child
+                max_ucb = child_ucb
 
         # Path to child chosen
         curr_node = max_child
@@ -170,3 +175,38 @@ def monte_carlo_tree_search(
             max_ucb_action = child.parent_action
     
     return max_ucb_action
+
+
+
+def render(board: dict[Coord, PlayerColor], use_color: bool=False, use_unicode: bool=False) -> str:
+    """
+    FOR DEBUGGING
+    Returns a visualisation of the game board as a multiline string, with
+    optional ANSI color codes and Unicode characters (if applicable).
+    """
+    def apply_ansi(str, bold=True, color=None):
+        bold_code = "\033[1m" if bold else ""
+        color_code = ""
+        if color == "r":
+            color_code = "\033[31m"
+        if color == "b":
+            color_code = "\033[34m"
+        return f"{bold_code}{color_code}{str}\033[0m"
+
+    output = ""
+    for r in range(11):
+        for c in range(11):
+            coord = Coord(r, c)
+            if coord in board:
+                color: PlayerColor = board[coord]
+                color = "r" if color == PlayerColor.RED else "b"
+                text = f"{color}"
+                if use_color:
+                    output += apply_ansi(text, color=color, bold=False)
+                else:
+                    output += text
+            else:
+                output += "."
+            output += " "
+        output += "\n"
+    return output
